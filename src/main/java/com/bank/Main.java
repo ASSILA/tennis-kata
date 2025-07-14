ffa8c667a08df3b5788079fc44adb84969a1b7e7
unknown <assilaali@gmail.com>
2025-07-15 01:03:33 +0200

    feat(console): add stdoutscore adapter and logic of the Main entrypoint

--- src/main/java/com/bank/Main.java
+++ src/main/java/com/bank/Main.java
@@ -1,7 +1,24 @@
 package com.bank;
 
+import com.bank.tennis.adapter.console.StdoutScore;
+import com.bank.tennis.application.BallWonCommand;
+import com.bank.tennis.application.TennisGameService;
+import com.bank.tennis.domain.Player;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length == 0) {
+            System.out.printf("Erreur : la séquence est obligatoire et ne doit contenir que les lettres A ou B.%n " +
+                    "Exemple : java Main ABABAA%n");
+            return;
+        }
+
+        var stdoutScore = new StdoutScore();
+        var service   = new TennisGameService(stdoutScore);
+
+        for (char c : args[0].toCharArray()) {
+            service.handle(new BallWonCommand(Player.fromChar(c)));
+            if (service.isFinished()) break;
+        }
     }
 }
